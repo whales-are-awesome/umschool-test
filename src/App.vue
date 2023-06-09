8cb21f21ec34bb4cd9d184e5fdc04a89c2455903
Nikita <suponov.nickita@gmail.com>
2023-06-10 02:57:09 +0300

    fixes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import { useRoute } from 'vue-router';
 import Layers from '@/components/Layers/Layers.vue';
 import { store } from '@/store';
 import useLayer from '@/hooks/useLayer';
@@ -13,16 +12,7 @@ import useLayer from '@/hooks/useLayer';
 
 // META
 
-const route = useRoute();
-
-const hasError = computed(() => !!store.state.error.status);
-
 const viewKeyCounter = computed(() => store.state.app.viewKey);
 
 useLayer();
-
-
-// ROUTE NAME
-
-const routeName = computed(() => route.name);
 </script>
